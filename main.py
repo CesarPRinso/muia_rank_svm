dba0d075d00992835f598bdfdfeb3d76cd50b70d
Cesar Pantoja <pantoja.rosales.inso@gmail.com>
2022-12-06 05:18:22 +0100

    first commit

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 import numpy as np
 import csv
-from baseline import baseline
+
+from data_preparation import build_learning_data_from
 from r_train import r_train
 from r_predict import r_predict
-from pca import pca
-
-# data
-train = np.arange(92)
-test = np.arange(92)
+import pandas as pd
+import pylab as pl
 
 x = []
-for t in csv.reader(open('x.csv', 'r')):
+for t in csv.reader(open('text/glucose_in_blood.csv', 'r')):
     x.append(t)
-x = np.asarray(x, 'f')
-# x=pca(x)
+x = np.asarray(x, dtype='object')
 
+rank1 = pd.read_csv('text/glucose_in_blood.csv', encoding="ISO-8859-1")
+
+# new_data_set = prepare_dataset(rank1, 'glucose in blood')  # data
+learning_data = build_learning_data_from(rank1)
+# learning_data = learning_data.to_numpy()
+train = learning_data.iloc[:50, :]
+test = learning_data.iloc[50:, :]
 y = []
-for t in csv.reader(open('y.csv', 'r')):
+for t in csv.reader(open('text/y.csv', 'r')):
     y.append(t)
 y = np.asarray(y, 'd')
 
 # train
-rsvm = r_train(x[train], y[train])
+rsvm = r_train(train, y[:50, :])
 # rank
-r = r_predict(rsvm, x[test])
-# r=baseline(x[test],x[-4:])
+r = r_predict(rsvm, test)
 
-import pylab as pl
 
 pl.scatter(r[:, 0], r[:, 1])
 pl.plot([0, len(r)], [r[4, 1], r[4, 1]], 'k--', lw=2)
